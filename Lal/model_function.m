4538d310b1188087f1b92121878bd69ea3769711
Vaishu1293 <clearexamseasy@gmail.com>
2023-04-26 16:13:55 +0100

    v5

--- Lal/model_function.m
+++
@@ -1,9 +0,0 @@
-function mod = model_function(xtrain,ytrain,hidden,epochs) 
-trainFcn = 'trainscg'; 
-hiddenLayerSize = hidden; 
-net = patternnet(hiddenLayerSize, trainFcn); 
-net=configure(net,xtrain,ytrain); 
-net.trainParam.epochs=epochs; 
-[net,tr] = train(net,xtrain,ytrain); 
-mod=net; 
-end
--- Lal/model_function_with_diff_optim.m
+++
@@ -1,9 +0,0 @@
-function mod = model_function_with_diff_optim(xtrain,ytrain,hidden,epochs) 
-%trainFcn = 'trainlm'; 
-trainFcn='trainrp'; 
-hiddenLayerSize = hidden; 
-net = patternnet(hiddenLayerSize, trainFcn); 
-net=configure(net,xtrain,ytrain); 
-net.trainParam.epochs=epochs; 
-%[net,tr] = train(net,xtrain,ytrain); 
-mod=net;nd
